4bd132a50dba006095d2a3175c4d505c5c0a7045
My Name <kiykobogdan@gmail.com>
2023-03-13 09:37:18 +0200

    Add project files.

---
+++ GenericMethod/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{
+    class Math
+    {
+        int x, y;
+        
+        public void Swap<T>(ref T od1,ref T od2)
+        {
+            T temp = od1;
+            od1 = od2;
+            od2 = temp;
+            Console.WriteLine($"x={od1}\ny={od2}");
+        }
+    }
+}
---
+++ GenericMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mat1 = new Math();
+            mat1.Swap<int>(ref 5,ref 6);
+            mat1.Swap<string>(ref "Hello", ref "Privet");
+            Console.ReadLine();
+        }
+    }
+}
